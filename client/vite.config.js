0099f24a677c115a7fbd85fddc4c732a1a1be27f
Your Name <23b01a4540@svecw.edu.in>
2026-05-30 11:25:33 +0530

    minor change

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/client/' : '/',
   plugins: [react()],
   server: {
     port: 5173,
